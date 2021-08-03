e58eecf7bf6a9ef9fced8031fe9b292e6180950b
Motti789 <mchillywilly@gmail.com>
2021-08-03 14:13:09 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,13 +1,22 @@
 class Triangle
+  attr_reader :base, :height, :side
   
 
   def initialize(base, height, side)
+    @base = base
+    @height = height
+    @side = side
   end
   
   
   def kind
    :equilateral
+   
   end  
+
+  # class TriangleError < StandardError
+  #   # triangle error code
+  # end
 
 
 end
